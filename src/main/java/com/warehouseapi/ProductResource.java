f2fff1df77caa13ecddcfa837561ca4ced4eb28b
a-jaxell <97404672+a-jaxell@users.noreply.github.com>
2023-10-08 21:02:19 +0200

    Add getProducts endpoint

--- src/main/java/com/warehouseapi/ProductResource.java
+++ src/main/java/com/warehouseapi/ProductResource.java
@@ -1,21 +1,35 @@
 package com.warehouseapi;
 
+import com.warehouse.entities.Product;
+import com.warehouse.entities.ProductCategory;
+import com.warehouse.entities.ProductRecord;
 import com.warehouseapi.service.WarehouseService;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import java.util.List;
 
 @Path("/products")
 public class ProductResource {
+    Product product = new Product("name", ProductCategory.UTENSILS, 2);
 
     private WarehouseService warehouseService;
     public ProductResource(){}
     @Inject
     public ProductResource(WarehouseService warehouseService){ this.warehouseService = warehouseService; };
     @GET
-    @Produces("text/plain")
-    public String hello() {
-        return "Hello, World!";
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getProducts() {
+        List<ProductRecord> result;
+        try{
+            result = warehouseService.getProducts();
+        }catch (Exception e) {
+            return Response.serverError().entity("Error: "+ e).build();
+        }
+        return Response.ok(result, MediaType.APPLICATION_JSON_TYPE).build();
     }
 }
