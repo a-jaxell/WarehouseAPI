4a9b8646443b2b421f6754e4e52ce9edecf6105f
a-jaxell <97404672+a-jaxell@users.noreply.github.com>
2023-10-10 17:25:15 +0200

    Add logging to addNewProduct

--- src/main/java/com/warehouseapi/ProductResource.java
+++ src/main/java/com/warehouseapi/ProductResource.java
@@ -6,8 +6,12 @@ import com.warehouse.entities.ProductRecord;
 import com.warehouseapi.service.WarehouseService;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.UriInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,6 +20,7 @@ import java.util.UUID;
 @Path("/products")
 public class ProductResource {
     Product product = new Product("name", ProductCategory.UTENSILS, 2);
+    private static final Logger logger = LoggerFactory.getLogger(ProductResource.class);
 
     private WarehouseService warehouseService;
     public ProductResource(){}
@@ -59,19 +64,32 @@ public class ProductResource {
     @POST
     @Path("/add")
     @Produces(MediaType.APPLICATION_JSON)
-    public  Response addNewProduct(@QueryParam("name") String name,
-                                   @QueryParam("productCategory") String productCategory,
-                                   @QueryParam("rating") int rating){
-            ProductRecord response;
+    public  Response addNewProduct(
+                @Context UriInfo uri,
+                @QueryParam("name") String name,
+                @QueryParam("productCategory") String productCategory,
+                @QueryParam("rating") int rating){
+
+        ProductRecord response;
+        logger.info("Connection to : "+ uri.getAbsolutePath());
 
         try{
             Product newProduct = new Product(name, Enum.valueOf(ProductCategory.class, productCategory.toUpperCase()), rating);
             response = ProductRecord.returnRecord(newProduct);
                     //warehouseService.addNewProduct(newProduct);
-
-        }catch (Exception e) {
-            return Response.serverError().entity("Error: "+ e).build();
+            if(name == null) {
+                throw new WebApplicationException(
+                        Response.status(Response.Status.BAD_REQUEST)
+                                .entity("name parameter is required")
+                                .build()
+                );
+            }
+        } catch (Exception e) {
+            logger.error("Error: " + e);
+            // This need to be fixed so that this either sends the exception status or
+            return Response.status(Response.Status.NOT_ACCEPTABLE).entity("Error: "+ e.getMessage()).build();
         }
+        logger.info("Send response: " + response);
         return Response.ok(response, MediaType.APPLICATION_JSON).build();
     }
 
