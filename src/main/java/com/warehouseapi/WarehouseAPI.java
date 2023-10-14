61170c0df93e282e30931fe46a688bbec1dc0171
a-jaxell <97404672+a-jaxell@users.noreply.github.com>
2023-10-14 21:42:37 +0200

    Add custom Not found exception mapper

--- src/main/java/com/warehouseapi/WarehouseAPI.java
+++ src/main/java/com/warehouseapi/WarehouseAPI.java
@@ -3,6 +3,16 @@ package com.warehouseapi;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @ApplicationPath("/api")
 public class WarehouseAPI extends Application {
+
+    @Override
+    public Set<Class <?>> getClasses() {
+        Set<Class <?>> classes = new HashSet<>();
+        classes.add(NotFoundExceptionMapper.class);
+        return classes;
+    }
 }
